4e5789f1ab6de79ec023c43ae02c34e704825dbc
Baraa Orabi <baraaorabi@gmail.com>
2022-01-31 10:57:05 -0800

    Major update for trie

--- py/match_lr_bc-trie.py
+++ py/match_lr_bc-trie.py
@@ -3,13 +3,12 @@ import sys
 import argparse
 import gzip
 import math
+import psutil
+from multiprocessing import Pool,RawArray
 
-import pandas as pd
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-import psutil
-from multiprocessing import Pool
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -61,12 +60,11 @@ def parse_args():
     
     return args
 
-map_char = [None for i in range(128)]
+map_char = [0 for _ in range(128)]
 map_char[ord('A')] = 0
 map_char[ord('C')] = 1
 map_char[ord('G')] = 2
 map_char[ord('T')] = 3
-map_char[ord('N')] = 4
 
 rev_compl_l = [chr(i) for i in range(128)]
 rev_compl_l[ord('A')] = 'T'
@@ -74,23 +72,29 @@ rev_compl_l[ord('C')] = 'G'
 rev_compl_l[ord('G')] = 'C'
 rev_compl_l[ord('T')] = 'A'
 
-trie = None
+alphabet_size = 4 
 barcodes = None
+barcodes_rc = None
+lr_segs = None
+lr_segs_idxs = None
+lr_names = None
+
 def rev_compl(s):
     return ''.join(rev_compl_l[ord(c)] for c in reversed(s))
 
-class Trie(object):
+def mem_use_gb():
+    return psutil.Process().memory_info().rss/(1024**3)
 
+class Trie(object):
     def __init__(self, max_error, barcode_length, capacity=100_000_000):
         self.nid_to_rids = [list()]
         self.nid_to_child = np.zeros(
-            (5,capacity),
+            (alphabet_size,capacity),
             dtype=np.uint32,
         )
         self.size = 1
         self.max_error = max_error
         self.barcode_length = barcode_length
-
     def __increase_capacity__(self):
         self.nid_to_child = np.concatenate(
             (
@@ -102,8 +106,7 @@ class Trie(object):
             ),
             axis = 1
         )
-        print(f'Doubled capacity. New shape: {self.nid_to_child.shape}')       
-
+        print(f'Doubled capacity. New shape: {self.nid_to_child.shape}')
     def insert(self, word, read_id):
         nid = 0
         for index, char in enumerate(word):
@@ -119,7 +122,6 @@ class Trie(object):
             nid = self.nid_to_child[cid][nid]
             if index >= self.barcode_length - self.max_error - 1:
                 self.nid_to_rids[nid].append(read_id)
-
     def dfs(self, nid, barcode, error, index, result):
         if index == len(barcode):
             edit_distance = self.max_error - error
@@ -130,7 +132,7 @@ class Trie(object):
         # delete
         if error > 0:
             self.dfs(nid, barcode, error - 1, index + 1, result)
-        for cid in range(5):
+        for cid in range(alphabet_size):
             child_id = self.nid_to_child[cid][nid]
             if child_id == 0:
                 continue
@@ -143,7 +145,6 @@ class Trie(object):
             # insert
             if error > 0:
                 self.dfs(child_id, barcode, error - 1, index, result)
-
     def query(self, barcode):
         result = [list() for _ in range(self.max_error+1)]
         self.dfs(
@@ -156,60 +157,114 @@ class Trie(object):
         return result
 
 def read_long_reads(path):
+    global lr_segs_idxs, lr_segs, lr_names
+    print('Reading long reads barcodes')
+    if path.endswith('.gz'):
+        f = gzip.open(path, 'rt')
+    else:
+        f = open(path, 'r')
+    lr_segs_tmp = list()
+    lr_names = list()
+    for l in f:
+        l = l.rstrip('\n').split('\t')
+        lr_names.append(l[0])
+        lr_segs_tmp.append(l[3])
 
-    names=["read_id", "distance", "strand", "segment"]
-    print('Reading long reads segments')
-    long_segment = pd.read_csv(path, delimiter = "\t", names=names)
+    N = sum(len(s) for s in lr_segs_tmp)
+    lr_segs = RawArray('c', N)
+    lr_segs_idxs = RawArray('i', len(lr_segs_tmp)+1)
+    a = 0
+    b = 0
+    for s in lr_segs_tmp:
+        lr_segs_idxs[b] = a
+        for c in s:
+            lr_segs[a]=ord(c)
+            a+=1
+        b+=1
+    lr_segs_idxs[b] = a
+    print(f'There are {len(lr_names):,} LRs')
 
-    return long_segment
-
-def read_short_reads(path):
+def read_sr_barcodes(path):
     print('Reading short reads barcodes')
-    global barcodes
+    global barcodes,barcodes_rc
     if path.endswith('.gz'):
         f = gzip.open(path, 'rt')
     else:
         f = open(path)
-    barcodes = [l.rstrip().split('\t')[0] for l in f]
-    print(f'There {len(barcodes)} SR barcodes')
+    barcodes = [l.rstrip('\n').split('\t')[0] for l in f]
+    barcodes_rc = [rev_compl(b) for b in barcodes]
+    print(f'There are {len(barcodes):,} SR barcodes')
 
-def trie_run_query(bid):
-    result = list()
-    for e,rids in enumerate(trie.query(barcodes[bid])):
-        result.append((e,True,rids))
-    for e,rids in enumerate(trie.query(rev_compl(barcodes[bid]))):
-        result.append((e,False,rids))
-    return bid,result
-
-def trie_search(read_total, threads):
-    result = [(100,set()) for _ in range(read_total)]
-    with Pool(threads) as p:
-        for bid,X in tqdm(p.imap(trie_run_query, range(len(barcodes))), total=len(barcodes)):
-            for (e,strand,rids) in X:
-                for rid in rids:
-                    if result[rid][0] > e:
-                        result[rid] = (e,set())
-                    if result[rid][0] == e:
-                        result[rid][1].add((bid,strand))
+def trie_search(trie):
+    result = dict()
+    for bid in range(len(barcodes)):
+        for e,rids in enumerate(trie.query(barcodes[bid])):
+            for rid in rids:
+                if not rid in result:
+                    result[rid] = (
+                        len(barcodes[0])+1, # Edit dist
+                        set(),              # Tuples of (bid,strand)
+                    )
+                if  e < result[rid][0]:
+                    result[rid] = (e,set())
+                if  e == result[rid][0]:
+                    result[rid][1].add((bid,True))
+        for e,rids in enumerate(trie.query(barcodes_rc[bid])):
+            for rid in rids:
+                if not rid in result:
+                    result[rid] = (
+                        len(barcodes[0])+1, # Edit dist
+                        set(),              # Tuples of (bid,strand)
+                    )
+                if  e < result[rid][0]:
+                    result[rid] = (e,set())
+                if  e == result[rid][0]:
+                    result[rid][1].add((bid,False))
     return result
 
-def run_get_matches_memory(long_reads, max_error, barcode_length, memory_GB, threads):
-    global trie
+def get_matches_prefix(args):
+    max_error, prefix = args
+    barcode_length = len(barcodes[0])
     trie = Trie(max_error, barcode_length)
+    trie.insert(prefix.decode(), -1)
     result = {}
-    print('Building trie')
-    for index, row in tqdm(long_reads.iterrows(), total=long_reads.shape[0]):
-        segment = row["segment"]
-        if type(segment)!=str and math.isnan(segment):
-            continue
+    for rid, (idx_1,idx_2) in enumerate(zip(lr_segs_idxs[:-1],lr_segs_idxs[1:])):
+        segment = lr_segs[idx_1:idx_2]
         for i in range(len(segment)):
             sub_segment = segment[i:i + barcode_length + max_error]
-            if len(sub_segment) >= barcode_length - max_error:
-                trie.insert(sub_segment, index)
-    print(f'Memory use: {psutil.Process().memory_info().rss / (1024**3):.2f}GB')
-    result = trie_search(long_reads.shape[0], threads)
+            if sub_segment.startswith(prefix) and len(sub_segment) >= barcode_length - max_error:
+                trie.insert(sub_segment.decode(), rid)
+    result = trie_search(trie)
     del trie
-    print(f'Memory use: {psutil.Process().memory_info().rss / (1024**3):.2f}GB')
+    return result
+
+def run_get_matches_prefix(max_error, threads):
+    prefixes = [b'']
+    while threads > len(prefixes):#*.5:
+        new_prefixes = list()
+        for p in prefixes:
+            for c in 'ACGT':
+                new_prefixes.append(
+                    (p.decode()+c).encode()
+                )
+            prefixes = new_prefixes
+    args = [(max_error,p) for p in prefixes]
+    print(args)
+    result = dict()
+    print(f'Running get_matches_prefix on {threads} threads and {len(args)} tasks')
+    with Pool(threads) as p:
+        for proc_result in tqdm(p.imap(get_matches_prefix, args, chunksize=1), total=len(args)):
+            for rid,(e,X) in proc_result.items():
+                if not rid in result:
+                    result[rid] = (
+                        len(barcodes[0])+1, # Edit dist
+                        set(),              # Tuples of (bid,strand)
+                    )
+                if  e < result[rid][0]:
+                    result[rid] = (e,set())
+                if  e == result[rid][0]:
+                    for bid,strand in X:
+                        result[rid][1].add((bid,strand))
     return result
 
 
@@ -249,38 +304,41 @@ def show_plot(full_data, plotfile, max_error):
 
     plt.savefig(plotfile)
 
-
 def main():
     args = parse_args()
     print(args)
-    global barcodes
-    read_short_reads(args.short_read_barcodes)
+    read_sr_barcodes(args.short_read_barcodes)
+    barcode_lens = {len(b) for b in barcodes}
+    assert barcode_lens == {args.barcode_length}, barcode_lens
+    del barcode_lens
+
     long_reads = read_long_reads(args.long_read_segments)
-    result = run_get_matches_memory(
-        long_reads=long_reads,
+    result = run_get_matches_prefix(
         max_error=args.max_error,
-        barcode_length=args.barcode_length,
-        memory_GB=args.mem,
         threads=args.threads,
     )
-    memory_useage_GB = psutil.Process().memory_info().rss / (1024**3)
+    memory_useage_GB = mem_use_gb()
     print(f"Memory usage: {memory_useage_GB:.2f}GB")
     # if args.plotfile != None:
     #     show_plot(result, args.plotfile, args.max_error)
     if args.outfile:
-        outfile = gzip.open(args.outfile, 'wt')
+        if args.outfile.endswith('gz'):
+            outfile = gzip.open(args.outfile, 'wt+')
+        else:
+            outfile = open(args.outfile, 'w+')
     else:
         outfile = sys.stdout
-    for rid,(e,bids) in tqdm(enumerate(result),total=len(result)):
-        segment = long_reads.iloc[rid]["segment"]
-        if type(segment)!=str and math.isnan(segment):
-            continue
-        outfile.write(f'{long_reads.iloc[rid]["read_id"]}\t')
+    for rid,(e,bids) in tqdm(sorted(result.items()),total=len(result)):
+        name = lr_names[rid]
+        idx_1 = lr_segs_idxs[rid]
+        idx_2 = lr_segs_idxs[rid+1]
+        seg = lr_segs[idx_1:idx_2].decode() 
+        outfile.write(f'{name}\t')
         if len(bids) == 0:
             e = 'inf'
         outfile.write(f'{e}\t')
         outfile.write(f'{len(bids)}\t')
-        outfile.write(f'{segment}\t')
+        outfile.write(f'{seg}\t')
         outfile.write(f'{",".join([barcodes[b] if s else rev_compl(barcodes[b]) for b,s in sorted(bids)])}\n')
     outfile.close()
 
